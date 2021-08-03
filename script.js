3cd73838aebe587f564bfccd6a67f2e3bc73c2c9
Mohamad Antar <Antar@Mohamads-MacBook-Air.local>
2021-08-03 22:00:12 +1000

    added Winner checking functionality and some styling

--- script.js
+++ script.js
@@ -34,14 +34,15 @@ function playerChooser() {
 // function playerTracker () {
 // }
 
-//Event Listener for Div boxes to add X or O
+//Event Listener for Div boxes to add X or O 
+//NEED TO ADD A STOP CONDITION AFTER COUNTER++ to PREVENT CHANGES IN SAME DIV
 playerChooser();
 for (let i = 0; i < divEntry.length; i++) {
     divEntry[i].addEventListener("click", (e) => {
-      if (counter1 <= counter2) { 
-        divEntry[i].innerHTML = player1;
-        counter1++;
-    } else if (counter1 > counter2) {
+        if (counter1 <= counter2) {
+            divEntry[i].innerHTML = player1;
+            counter1++;
+        } else if (counter1 > counter2) {
             if (divEntry.textContent !== 'X' && divEntry.textContent !== 'O') {
                 divEntry[i].innerHTML = player2
                 counter2++
@@ -49,3 +50,42 @@ for (let i = 0; i < divEntry.length; i++) {
         }
     })
 }
+
+//Win Checker - loop through all divs
+let winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
+
+
+function winChecker() {
+    for (let i = 0; i < winConditions.length; i++) {
+        winConditionsA = winConditions[i];
+        let sumX = 0;
+        let sumO = 0;
+        console.log(winConditions[i])
+        for (const conditions of winConditionsA) {
+            console.log(divEntry[conditions])
+            if (divEntry[conditions].textContent === 'X') {
+                sumX++
+            } 
+            if (divEntry[conditions].textContent === 'O') {
+                sumO++
+            } 
+        } 
+        if (sumX === 3) {
+            alert('X is the Winner')
+        } else if (sumO === 3) {
+            alert ('O is the Winner')
+        }
+    } 
+    
+}
+
+
