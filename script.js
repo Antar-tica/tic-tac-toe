e35044d74076509f50c9a430fd35fb4fe7a334e6
Mohamad Antar <Antar@Mohamads-MacBook-Air.local>
2021-08-07 12:54:32 +1000

    fixed up reset button and added some styling to the page

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ let b8 = document.getElementById("b8").value;
 let b9 = document.getElementById("b9").value;
 let player1Score = document.getElementById("player1Score");
 let player2Score = document.getElementById("player2Score");
+let selector = document.getElementById("selector");
 
 let divEntry = document.getElementsByClassName("box");
+let button = document.getElementById('button');
+let winNotifier = document.getElementById('winNotifier');
 
 //Variables
 counter1 = 0;
@@ -25,6 +28,7 @@ let playerRandomiser = Math.floor(Math.random() * 2 + 1);
 function playerChooser() {
     if (playerRandomiser === 1) {
         player1 = 'X'
+        // selector + '[X]'
         player2 = 'O'
     } else {
         player1 = 'O'
@@ -33,18 +37,23 @@ function playerChooser() {
 }
 
 //Update Player Score
-function updatePlayerScore (player) {
-    if (player === player1) {
-        let currentScore = Number(player1Score.innerText);
-        player1Score.innerHTML = currentScore + 1
-    }
-    if (player === player2) {
-        let currentScore = Number(player2Score.innerText);
-        player2Score.innerHTML = currentScore + 1
-    }
+function updatePlayerScore(player) {
+    if ('X' === player1) {
+        let currentScore1 = Number(player1Score.innerText);
+        player1Score.innerHTML = currentScore1 + 1
+    } else if ('X' === player2) {
+        let currentScore2 = Number(player2Score.innerText);
+        player2Score.innerHTML = currentScore2 + 1
+    } 
 }
 
 //function to reset grid
+button.addEventListener("click", (e) => {
+    for (let i = 0; i < divEntry.length; i++) {
+        divEntry[i].innerHTML = ""
+    }
+    winNotifier.innerHTML = ""
+});
 
 
 //Event Listener for Div boxes to add X or O 
@@ -53,15 +62,15 @@ let currentPlayer = player1;
 for (let i = 0; i < divEntry.length; i++) {
     divEntry[i].addEventListener("click", (e) => {
         const isEmpty = !divEntry[i].textContent;
-        if (!isEmpty){
-        return;
+        if (!isEmpty) {
+            return;
         }
         if (currentPlayer === player1) {
             divEntry[i].innerHTML = player1;
             currentPlayer = player2;
         } else if (currentPlayer === player2) {
             divEntry[i].innerHTML = player2;
-            currentPlayer = player1;     
+            currentPlayer = player1;
         }
 
         winChecker();
@@ -91,20 +100,18 @@ function winChecker() {
             console.log(divEntry[conditions])
             if (divEntry[conditions].textContent === 'X') {
                 sumX++
-            } 
+            }
             if (divEntry[conditions].textContent === 'O') {
                 sumO++
-            } 
-        } 
+            }
+        }
         if (sumX === 3) {
             updatePlayerScore('X');
-            alert('X is the Winner')
+            winNotifier.innerHTML = `${player1} is the Winner`
         } else if (sumO === 3) {
             updatePlayerScore('O');
-            alert ('O is the Winner')
+            winNotifier.innerHTML = `${player2} is the Winner`
         }
-    } 
-    
+    }
+
 }
-
-
